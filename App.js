2e929ba5778996571accb634918a2ad8debfda42
Joshua <amjoshua59@gmail.com>
2024-05-31 15:35:41 +0000

    Created categories section

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
+import Categories from "./components/Categories";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
       <SearchBar />
+      <Categories />
     </View>
   );
 }
@@ -16,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F7F0E8",
-    gap: 10,
+    gap: 20,
     paddingTop: 10,
     paddingHorizontal: 15,
   },
